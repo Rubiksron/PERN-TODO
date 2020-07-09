bfcb8c7987e06b0a60c740b3f6f46b699b6572f0
Rubiksron <rubiksron@gmail.com>
2020-07-09 13:25:10 -0700

    added an app folder in which i placed the client folder.

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json()); //lets us access req.body
 
 //needed for depoloying to provide the index.html as the source to build from
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(`${__dirname}/build/index.html`)));
 app.get('/*', function (req, res) {
   res.sendFile(path.join(`${__dirname}/build/index.html`));
 });
