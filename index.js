ede2a706e7d0f9f242d4a73c6edfe8768533434d
Rubiksron <rubiksron@gmail.com>
2020-07-09 11:50:17 -0700

    brought package.json out from the server folder into the root.  added a start script that will first run nodemon then cd into the client folder and run npm start.

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 const cors = require('cors');
-const pool = require('./db.js');
+const pool = require('./server/db.js');
 
 //middleware 
 app.use(cors());
 app.use(express.json()); //lets us access req.body
+
+//needed for depoloying to provide the index.html as the source to build from
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 //routes//
 
